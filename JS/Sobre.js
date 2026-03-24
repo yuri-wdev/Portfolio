700e730986eff42dd3cc020bb47af99c5cd06486
Yuri <yuriaragao0511@gmail.com>
2026-03-23 22:23:52 -0300

    Pop-ups página sobre

---
+++ JS/Sobre.js
@@ -0,0 +1,16 @@
+function abrirservicos(){
+    document.getElementById('servicos').style.display = 'block';
+}
+
+function fecharservicos(){
+    document.getElementById('servicos').style.display = 'none';
+}
+
+function abrirtrabalhos(){
+    document.getElementById('trabalhos').style.display = 'block';
+}
+
+function fechartrabalhos(){
+    document.getElementById('trabalhos').style.display = 'none';
+}
+
