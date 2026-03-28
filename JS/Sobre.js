35e093b35b46272199bdbe9dc23a54c96124d087
Yuri <yuriaragao0511@gmail.com>
2026-03-28 14:59:12 -0300

    Ajustando popups

--- JS/Sobre.js
+++ JS/Sobre.js
@@ -1,16 +1,16 @@
 function abrirservicos(){
-    document.getElementById('servicos').style.display = 'block';
+    document.getElementById('pop2').style.display = 'block';
 }
 
 function fecharservicos(){
-    document.getElementById('servicos').style.display = 'none';
+    document.getElementById('pop2').style.display = 'none';
 }
 
 function abrirtrabalhos(){
-    document.getElementById('trabalhos').style.display = 'block';
+    document.getElementById().style.display = 'block';
 }
 
 function fechartrabalhos(){
-    document.getElementById('trabalhos').style.display = 'none';
+    document.getElementById().style.display = 'none';
 }
 
